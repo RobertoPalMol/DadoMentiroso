0c8344f5325daba73b05e0a9cbd8cbf17ed0b645
robpalmol <robpalmol@alu.edu.gva.es>
2024-12-09 12:09:54 +0100

    Initial commit

---
+++ src/GenerarDado.java
@@ -0,0 +1,7 @@
+public class GenerarDado {
+
+    static int Dado(int dado){
+        dado=(int)(Math.random()*6+1);
+        return dado;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] tusDados = new int[0];
+        int dadosRestantes=5;
+
+        while(dadosRestantes>0){
+
+            for (int i=0; i==dadosRestantes; i++){
+                GenerarDado.Dado(tusDados[i]);
+            }
+        }
+    }
+}
